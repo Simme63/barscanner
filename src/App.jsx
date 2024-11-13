1b27071e4d59cfdcc1247b5c825a8b442c1ccbcc
Simme63 <68356343+Simme63@users.noreply.github.com>
2024-11-13 09:31:49 +0100

    Revert "milk"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import Youtube from "./components/Youtube";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<div>
-			<h1>Barcode</h1>
-			<Youtube></Youtube>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Barcode</h1>
+    </div>
+  );
 }
 
 export default App;
