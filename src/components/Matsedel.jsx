492d71bd88ff9a70882164794fd92ca2fd9bc20f
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2024-11-19 14:03:00 +0100

    Ring / Border...

--- src/components/Matsedel.jsx
+++ src/components/Matsedel.jsx
@@ -2,38 +2,35 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Mat() {
-	const { data } = useQuery({
-		queryKey: ["sigma"],
-		queryFn: async () => {
-			const response = await fetch(
-				`https://ntifoodpeople.vercel.app/api/food/day`
-			);
-			const data = await response.json();
-			return data;
-		},
-	});
+  const { data } = useQuery({
+    queryKey: ["sigma"],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://ntifoodpeople.vercel.app/api/food/day`
+      );
+      const data = await response.json();
+      return data;
+    },
+  });
 
-	if (data == undefined) return <>data pushed</>;
+  if (data == undefined) return <>data pushed</>;
 
-	return (
-		<div className="bg-purple-900 rounded-3xl p-10 w-1/3">
-			{data.items.map((item) => {
-				return (
-					<span
-						className="text-white font-black text-base"
-						key={item.title}
-					>
-						<h3 className="text-5xl">{item.title}</h3>
-						<br />
-						<p
-							dangerouslySetInnerHTML={{
-								__html: item.description,
-							}}
-						></p>
-						<br />
-					</span>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="bg-white rounded-3xl p-10 w-1/3 ring-16 ring-purple-900">
+      {data.items.map((item) => {
+        return (
+          <span className="text-white font-black text-base" key={item.title}>
+            <h3 className="text-5xl">{item.title}</h3>
+            <br />
+            <p
+              dangerouslySetInnerHTML={{
+                __html: item.description,
+              }}
+            ></p>
+            <br />
+          </span>
+        );
+      })}
+    </div>
+  );
 }
