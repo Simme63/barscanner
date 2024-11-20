acd3424b37afda4a09563698ca3560f14bcf2b1a
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2024-11-20 10:14:50 +0100

    text scale

--- src/components/Matsedel.jsx
+++ src/components/Matsedel.jsx
@@ -6,7 +6,7 @@ export default function Mat() {
     queryKey: ["sigma"],
     queryFn: async () => {
       const response = await fetch(
-        `https://ntifoodpeople.vercel.app/api/food/day`
+        `https://ntifoodpeople.vercel.app/api/food/week`
       );
       const data = await response.json();
       return data;
@@ -16,13 +16,14 @@ export default function Mat() {
   if (data == undefined) return <>data pushed</>;
 
   return (
-    <div className="bg-white rounded-3xl p-10 w-1/3 ring-16 ring-purple-900">
+    <div className="bg-white rounded-3xl p-10 mt-6 w-1/3 ring-16 ring-purple-900">
       {data.items.map((item) => {
         return (
-          <span className="text-black font-black text-base" key={item.title}>
+          <span className="text-black font-semibold text-base" key={item.title}>
             <h3 className="text-5xl">{item.title}</h3>
             <br />
             <p
+              className="font-light text-xl"
               dangerouslySetInnerHTML={{
                 __html: item.description,
               }}
