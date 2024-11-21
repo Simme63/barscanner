8cb4b6ec380cb8a5507a8d8ff115c0602002e472
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2024-11-21 11:11:03 +0100

    functional nav - ver ;3

--- src/App.jsx
+++ src/App.jsx
@@ -13,39 +13,39 @@ const queryClient = new QueryClient();
 export const RouteAtom = atom("/");
 
 function App() {
-	const [route, setRoute] = useAtom(RouteAtom);
-	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<Route path="/">
-					<div className="flex flex-col justify-between gap-5">
-						<div className="flex flex-row">
-							<List />
-							<div className="flex flex-col">
-								<DigitalClock />
-								<Mat />
-							</div>
-						</div>
-						<footer className="bg-purple-950 w-full h-20">
-							<div className="flex p-3 w-fit h-full bg-slate-300 hover:bg-cyan-500 justify-center">
-								<Navigate path="/statistics">
-									Statistics
-								</Navigate>
-							</div>
-						</footer>
-					</div>
-				</Route>
-				<Route path="/statistics">
-					<div className="flex flex-row justify-between gap-5">
-						<h1 className="text-[15rem] text-white">sigma</h1>
-					</div>
-					<div className="absolute bottom-10 text-white text-3xl">
-						<Navigate path="/">back</Navigate>
-					</div>
-				</Route>
-			</QueryClientProvider>
-		</>
-	);
+  const [route, setRoute] = useAtom(RouteAtom);
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Route path="/">
+          <div className="flex flex-col justify-between gap-5">
+            <div className="flex flex-row">
+              <List />
+              <div className="flex flex-col">
+                <DigitalClock />
+                <Mat />
+              </div>
+            </div>
+            <footer className="bg-indigo-950 w-full h-20">
+              <div className="flex p-3 w-fit h-full bg-slate-300 hover:bg-cyan-500 justify-center">
+                <Navigate path="/statistics">Statistics</Navigate>
+              </div>
+            </footer>
+          </div>
+        </Route>
+        <Route path="/statistics">
+          <div className="flex flex-row justify-between gap-5">
+            <h1 className="text-[15rem] text-white">sigma</h1>
+          </div>
+          <footer className="bg-indigo-950 w-full h-20">
+            <div className="flex p-5 w-fit h-full bg-slate-300 hover:bg-cyan-500 justify-center">
+              <Navigate path="/">back</Navigate>
+            </div>
+          </footer>
+        </Route>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
--- src/components/Matsedel.jsx
+++ src/components/Matsedel.jsx
@@ -2,56 +2,52 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Mat() {
-	const { data } = useQuery({
-		queryKey: ["sigma"],
-		queryFn: async () => {
-			const response = await fetch(
-				`https://ntifoodpeople.vercel.app/api/food/week`
-			);
-			const data = await response.json();
-			return data;
-		},
-	});
+  const { data } = useQuery({
+    queryKey: ["sigma"],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://ntifoodpeople.vercel.app/api/food/week`
+      );
+      const data = await response.json();
+      return data;
+    },
+  });
 
-	if (data == undefined) return <>data pushed</>;
-	let today = new Date().getDay();
+  if (data == undefined) return <>data pushed</>;
+  let today = new Date().getDay();
 
-	function getWeekday(apiString) {
-		return apiString.split(" - ")[0];
-	}
+  function getWeekday(apiString) {
+    return apiString.split(" - ")[0];
+  }
 
-	return (
-		<div className="bg-purple-950 rounded-3xl p-10 mt-6 mr-28 ring-16 ring-purple-900">
-			{data.items.map((item, index) => {
-				return (
-					<span
-						className={`font-medium text-base ${
-							today - 1 === index
-								? `text-orange-400`
-								: `text-white`
-						}`}
-						key={item.title}
-					>
-						<h3
-							className={`text-l ${
-								today - 1 === index
-									? `text-orange-400`
-									: `text-white`
-							}`}
-						>
-							{getWeekday(item.title)}
-						</h3>
-						<br />
-						<p
-							className="font-light text-lg"
-							dangerouslySetInnerHTML={{
-								__html: item.description,
-							}}
-						></p>
-						<br />
-					</span>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="bg-indigo-950 rounded-3xl p-10 mt-5	 ring-16 ring-violet-950">
+      {data.items.map((item, index) => {
+        return (
+          <span
+            className={`font-medium text-base ${
+              today - 1 === index ? `text-orange-400` : `text-white`
+            }`}
+            key={item.title}
+          >
+            <h3
+              className={`text-l ${
+                today - 1 === index ? `text-orange-400` : `text-white`
+              }`}
+            >
+              {getWeekday(item.title)}
+            </h3>
+            <br />
+            <p
+              className="font-light text-lg"
+              dangerouslySetInnerHTML={{
+                __html: item.description,
+              }}
+            ></p>
+            <br />
+          </span>
+        );
+      })}
+    </div>
+  );
 }
