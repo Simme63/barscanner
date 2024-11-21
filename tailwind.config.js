d7eec964da86dd79e2ce63b95e4e8b1dfc39961e
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2024-11-21 12:08:12 +0100

    stylings...

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
       fontSize: {
         l: "2.3rem",
       },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
     },
     plugins: [],
   },
